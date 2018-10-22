9d558ef20834f22d3e17a9c5f8112fea256e7b3f
VictorGoing <wiktor.idziak97@gmail.com>
2018-10-22 11:45:54 +0200

    Moduł 6. Wprowadzenie do testowania 2 mainy XD

---
+++ kodilla-test/src/main/java/com/kodilla/testing/User/SImpleUser.java
@@ -0,0 +1,15 @@
+package com.kodilla.testing.User;
+
+
+public class SImpleUser {
+    private String username;
+
+    public SImpleUser (String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
+
